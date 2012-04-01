12ac80f0411c48b6178bbc0ead993e11d9a57c0e
Nader Hendawi <naderhen@gmail.com>
2012-04-01 16:37:52 -0400

    deploy change

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,1 +1,5 @@
 server 'bluebase', :app, :web, :db, :primary => true
+set :default_environment, {
+  'LANG' => 'en_US.UTF-8',
+  'PATH' => '/usr/kerberos/sbin:/usr/sbin:/sbin:/usr/kerberos/bin:/usr/local/bin:/bin:/usr/bin'
+}
