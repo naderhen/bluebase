6926b72dd9389e0edccb0b166f6242b970c69405
Nader Hendawi <naderhen@gmail.com>
2012-04-01 16:40:03 -0400

    getting rid

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,1 @@
 server 'bluebase', :app, :web, :db, :primary => true
-set :bundle_cmd, "LANG='en_US.UTF-8' LC_ALL='en_US.UTF-8' bundle"
-set :default_environment, {
-  'LANG' => 'en_US.UTF-8',
-  'PATH' => '/usr/kerberos/sbin:/usr/sbin:/sbin:/usr/kerberos/bin:/usr/local/bin:/bin:/usr/bin'
-}
--- config/environment.rb
+++ config/environment.rb
@@ -3,4 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Bluebase::Application.initialize!
-Encoding.default_internal = 'UTF-8'
